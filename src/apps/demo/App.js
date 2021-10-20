632cedac2f622bade8262cb4ae6d2fb32d5eb9ca
Alexander Gaivoronskiy <a.gvrnsk@gmail.com>
2021-10-20 22:17:57 +0300

    build updated

--- src/apps/demo/App.js
+++ src/apps/demo/App.js
@@ -1,5 +1,4 @@
 import CoreApp from "core/App"
-import * as THREE from 'three';
 import AssetManager from "core/utils/AssetManager";
 
 class App extends CoreApp {
@@ -8,40 +7,13 @@ class App extends CoreApp {
             ...params,
         });
 
-        this.load_prefab(AssetManager.load_prefab("demo.kit_main_scene"))
+        this.load_prefab(AssetManager.load_prefab("{app_name}.scenes.kit_main_scene"))
         this.refs.renderer.target_fps = 60
-        this.find_component_of_type("CameraComponent").position = [
-            3,
-            -4,
-            3
-        ]
 
-        let postfx = this.find_component_of_type("Postprocessing")
-
-        console.log(postfx)
-
-        postfx.outline_selection = [
-            this.children[6].children[0].refs.mesh.get_render_data().object,
-            this.find_component_with_tag("cube2").get_render_data().object
-        ]
-
-    }
-    on_tick() {
-        let time = this.globals.uniforms.time.value
         let camera = this.find_component_of_type("CameraComponent")
-        if (camera) {
-            camera.fov = 80 + (Math.sin(time) * 25)
-            let radius = 2 + Math.abs(Math.sin(time / 10)) * 5
-            camera.position = [
-                Math.sin(time / 5) * radius,
-                Math.sin(time / 7) * 5,
-                Math.cos(time / 11) * radius
-            ]
-
-            this.find_component_of_type("CameraComponent").look_at(0, 0, 0)
-        }
-
+        camera.fov = 60
     }
+    on_tick() { }
 }
 
 export default App;
