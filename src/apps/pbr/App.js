345eaf59031cde4885d6051a30050de63107e9ce
Alexander Gaivoronskiy <a.gvrnsk@gmail.com>
2021-11-09 17:37:14 +0200

    pbr scene

---
+++ src/apps/pbr/App.js
@@ -0,0 +1,27 @@
+import CoreApp from "core/App"
+import ResourceManager from "core/ResourceManager";
+
+class App extends CoreApp {
+    constructor(params) {
+        super({
+            ...params,
+        });
+        let stage = this.load_stage(ResourceManager.load_prefab("{{APP_NAME}}.scenes.main"))
+        let renderer = this.find_component_of_type("Renderer")
+        renderer.target_fps = 60
+        let camera = this.find_component_of_type("CameraComponent")
+        camera.position = [
+            0,
+            2,
+            5
+        ]
+        camera.fov = 60
+        let controls = this.find_component_of_type("OrbitControlsComponent")
+        controls.set_controls_state({ "target": [0, 0, 0], "position": [3.756349266102919e-15, -0.026115050734834725, 4.6170318205801735], "zoom": 1 })
+
+        this.start()
+    }
+    on_tick() { }
+}
+
+export default App;
---
+++ src/apps/pbr/components/ModelViewerController.js
@@ -0,0 +1,101 @@
+import Component from "core/Component";
+import ResourceManager from "core/ResourceManager";
+import * as THREE from 'three';
+import { log, error, is_none, console } from "core/utils/Tools"
+import { isString, isObject, isFunction, isArray, isNumber, isBoolean, isUndefined, isNull, map, filter, keys, values, set, get, unset, throttle, debounce } from "lodash-es"
+import Schema from "core/utils/Schema"
+
+class ModelViewerController extends Component {
+    radius = 5
+    current_model = 0
+    /**private */
+    target_rotation = undefined
+    tick_rate = 60
+    constructor() {
+        super(...arguments)
+        this.target_rotation = [0, 0, 0]
+        //@TODO:
+        this.next_model = debounce(this.next_model, 199)
+        this.prev_model = debounce(this.prev_model, 199)
+
+    }
+    get_reactive_props() {
+        return [
+            "current_model",
+            ...super.get_reactive_props()
+        ]
+    }
+    on_update(props) {
+        super.on_update(props)
+        props.forEach(prop => {
+            switch (prop) {
+                case "current_model": {
+                    this.update_focus()
+                    this.update_caption()
+                    break
+                }
+            }
+        })
+    }
+    on_create() {
+        this.log(`created`)
+        this.align_items()
+        this.current_model = 0
+    }
+    align_items() {
+        let items_count = this.children.length;
+        let radius = this.radius
+        this.children.forEach((child, index) => {
+            let circle_progress = index / items_count;
+            let pos_x = Math.sin(circle_progress * Math.PI * 2) * radius
+            let pos_z = -Math.cos(circle_progress * Math.PI * 2) * radius
+            this.log(`setting model #${index + 1} position to [${pos_x}, ${pos_z}]`)
+            child.position = [
+                pos_x,
+                0,
+                pos_z
+            ]
+        })
+    }
+    update_caption() {
+        let caption = this.find_component_with_tag("current_model_title")
+        if (caption) {
+            caption.text = `#${this.current_model + 1}`
+        }
+    }
+    update_focus() {
+        let game_object = this.game_object;
+        game_object.position = [
+            0,
+            0,
+            -this.radius
+        ]
+
+        let current_game_object_rotation_y = this.game_object.rotation[1]
+        let new_game_object_roration_y = ((this.current_model / this.children.length) * Math.PI * 2) + Math.PI
+
+        let target_rotation = this.target_rotation = [
+            0,
+            new_game_object_roration_y,
+            0
+        ]
+    }
+    next_model() {
+        this.current_model = (this.current_model + 1) % this.children.length
+        this.log(`next_model`)
+    }
+    prev_model() {
+        this.current_model = (this.children.length + this.current_model - 1) % this.children.length
+        this.log(`prev_model`)
+
+    }
+    on_tick(time_delta) {
+        this.game_object.rotation = this.lerp(
+            this.game_object.rotation,
+            this.target_rotation,
+            0.8 * time_delta.delta
+        )
+    }
+}
+
+export default ModelViewerController;
