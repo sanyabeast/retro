fe921814d127d8f926847c1297272227cfc90144
Alexander Gaivoronskiy <a.gvrnsk@gmail.com>
2021-11-27 21:44:38 +0200

    tweaks

--- webpack/builds/webpack.config.dev.js
+++ webpack/builds/webpack.config.dev.js
@@ -6,5 +6,27 @@ module.exports = {
     devtool: "#source-map",
     plugins: [
         new BundleAnalyzerPlugin()
-    ]
+    ],
+    optimization: {
+        splitChunks: {
+            chunks: 'async',
+            minSize: 20000,
+            minChunks: 1,
+            maxAsyncRequests: 30,
+            maxInitialRequests: 30,
+            enforceSizeThreshold: 50000,
+            cacheGroups: {
+                defaultVendors: {
+                    test: /[\\/]node_modules[\\/]/,
+                    priority: -10,
+                    reuseExistingChunk: true,
+                },
+                default: {
+                    minChunks: 2,
+                    priority: -20,
+                    reuseExistingChunk: true,
+                },
+            },
+        }
+    }
 };
--- webpack/builds/webpack.config.prod.js
+++ webpack/builds/webpack.config.prod.js
@@ -35,6 +35,26 @@ module.exports = (params) => {
                     terserOptions: terser_options,
                 }),
             ],
+            splitChunks: {
+                chunks: 'async',
+                minSize: 20000,
+                minChunks: 1,
+                maxAsyncRequests: 30,
+                maxInitialRequests: 30,
+                enforceSizeThreshold: 50000,
+                cacheGroups: {
+                    defaultVendors: {
+                        test: /[\\/]node_modules[\\/]/,
+                        priority: -10,
+                        reuseExistingChunk: true,
+                    },
+                    default: {
+                        minChunks: 2,
+                        priority: -20,
+                        reuseExistingChunk: true,
+                    },
+                },
+            }
         },
         plugins: [
             new TerserPlugin({
